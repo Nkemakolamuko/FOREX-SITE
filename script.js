9a509b68f7668098ef692e9844cf486b1bdd3cd8
Nkemakolauko <nwekevino99@gmail.com>
2023-09-08 13:45:30 +0100

    Add link text and reduce contact button - fix margins

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ hiddenElements.forEach((el) => observer.observe(el))
         if (target.length) {
           $('html, body').stop().animate({
             scrollTop: target.offset().top - 100
-          }, 1000); 
+          }, 500); 
         }
     });
   });
@@ -43,3 +43,47 @@ const today = document.querySelectorAll('.today');
 today.forEach(elem => {
     elem.textContent = new Date().toDateString();
 })
+
+
+
+// Get all the navigation links with the common class
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Define options for the Intersection Observer
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.9, // Adjust this threshold for when the link becomes active
+};
+
+// Callback function for the Intersection Observer
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Remove the "active" class from all navigation links
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+      });
+
+      // Find the navigation link with the corresponding href
+      const activeLink = document.querySelector(`[href="#${entry.target.id}"]`);
+      
+      // Add the "active" class to the active navigation link
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    }
+  });
+}
+
+// Create an Intersection Observer instance
+const sectionObserver = new IntersectionObserver(handleIntersection, observerOptions);
+
+// Observe each section
+navLinks.forEach((link) => {
+  const targetId = link.getAttribute('href').substring(1); // Remove the "#" symbol
+  const targetSection = document.getElementById(targetId);
+  if (targetSection) {
+    sectionObserver.observe(targetSection);
+  }
+});
