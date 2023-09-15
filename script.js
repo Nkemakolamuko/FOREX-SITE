fc8c01a4a1f47e6253926864e9b3d0a2ae57822c
Nkemakolauko <nwekevino99@gmail.com>
2023-09-15 19:44:20 +0100

    Fix the hamburger not clicking

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show")
-        } else {
-            entry.target.classList.remove("show")
-        }
-    })
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add("show")
+      } else {
+          entry.target.classList.remove("show")
+      }
+  })
 })
 
 
@@ -15,33 +15,33 @@ hiddenElements.forEach((el) => observer.observe(el))
 
 
 
-  // Smooth Scroll
+// Smooth Scroll
 
-  $(document).ready(function() {
-      
-    $('a[href^="#"]').on('click', function(event) {
-        event.preventDefault();
-        var target = $(this.getAttribute('href'));
-        if (target.length) {
-          $('html, body').stop().animate({
-            scrollTop: target.offset().top - 100
-          }, 1000); 
-        }
-    });
+$(document).ready(function() {
+    
+  $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top - 100
+        }, 1000); 
+      }
   });
+});
 
 // Reseting Form values
 const form = document.querySelector('#my-Form');
 
 form.addEventListener('submit', () => {
-    form.reset();
+  form.reset();
 });
 
 // Updating date 
 const today = document.querySelectorAll('.today');
 
 today.forEach(elem => {
-    elem.textContent = new Date().toDateString();
+  elem.textContent = new Date().toDateString();
 })
 
 
@@ -51,29 +51,29 @@ const navLinks = document.querySelectorAll('.nav-link');
 
 // Define options for the Intersection Observer
 const observerOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.7, // Adjust this threshold for when the link becomes active
+root: null,
+rootMargin: '0px',
+threshold: 0.7, // Adjust this threshold for when the link becomes active
 };
 
 // Callback function for the Intersection Observer
 function handleIntersection(entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      // Remove the "active" class from all navigation links
-      navLinks.forEach((link) => {
-        link.classList.remove('active');
-      });
+entries.forEach((entry) => {
+  if (entry.isIntersecting) {
+    // Remove the "active" class from all navigation links
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+    });
 
-      // Find the navigation link with the corresponding href
-      const activeLink = document.querySelector(`[href="#${entry.target.id}"]`);
-      
-      // Add the "active" class to the active navigation link
-      if (activeLink) {
-        activeLink.classList.add('active');
-      }
+    // Find the navigation link with the corresponding href
+    const activeLink = document.querySelector(`[href="#${entry.target.id}"]`);
+    
+    // Add the "active" class to the active navigation link
+    if (activeLink) {
+      activeLink.classList.add('active');
     }
-  });
+  }
+});
 }
 
 // Create an Intersection Observer instance
@@ -81,11 +81,11 @@ const sectionObserver = new IntersectionObserver(handleIntersection, observerOpt
 
 // Observe each section
 navLinks.forEach((link) => {
-  const targetId = link.getAttribute('href').substring(1); // Remove the "#" symbol
-  const targetSection = document.getElementById(targetId);
-  if (targetSection) {
-    sectionObserver.observe(targetSection);
-  }
+const targetId = link.getAttribute('href').substring(1); // Remove the "#" symbol
+const targetSection = document.getElementById(targetId);
+if (targetSection) {
+  sectionObserver.observe(targetSection);
+}
 });
 
 
@@ -95,6 +95,6 @@ const navLink = document.querySelector('.nav-links');
 // const toggleClose = document.querySelector('.hamburgerMenuClose')
 
 toggleBtn.addEventListener('click', () => {
-  navLink.classList.toggle('open');
-  // navLink.classList.toggle('close');
+navLink.classList.toggle('open');
+// navLink.classList.toggle('close');
 });
